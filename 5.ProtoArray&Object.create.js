305afd013fe1fd9b20d06d89c7b10af3da9b03bb
Supriya Haldar <112514582+Supriya9002@users.noreply.github.com>
2023-09-27 13:26:41 +0530

    Add files via upload

---
+++ 5.ProtoArray&Object.create.js
@@ -0,0 +1,18 @@
+
+//Proto Array
+const inputs = [1, 2, 3, 4, 5];
+
+console.log(inputs.__proto__.__proto__);
+
+//Object create
+const car = {
+    getDetails(name) {
+      console.log(`Your car is ${name}`);
+    },
+  };
+  
+  const car1 = Object.create(car);
+  console.log(car1.__proto__);
+  car1.getDetails('Audi');
+  
+  //Object.assign
